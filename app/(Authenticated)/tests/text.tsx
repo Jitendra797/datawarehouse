8b07ee48a5903165f821782d09c0542263af56d8
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-07 00:31:58 +0530

    test 1

--- app/(Authenticated)/tests/text.tsx
+++ app/(Authenticated)/tests/text.tsx
@@ -3,12 +3,7 @@
 //   return <div>Hello</div>
 // }
 
-// MyComponent.jsx
-const MyComponent = () => {
-  return <div>Hello</div>
-}
-
-// export { MyComponent } //This is allowed.
+export { MyComponent } //This is allowed.
 // //export default { MyComponent }; //This is not allowed
 
 // function MyComponent() {
