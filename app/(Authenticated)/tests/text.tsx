2732b5d8c4c78e63b7a8625da432bd2f6a985fa8
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-07 00:33:57 +0530

    test 2

--- app/(Authenticated)/tests/text.tsx
+++ app/(Authenticated)/tests/text.tsx
@@ -1,11 +1,4 @@
-// // MyComponent.jsx
-// export function MyComponent() {
-//   return <div>Hello</div>
-// }
-
-export { MyComponent } //This is allowed.
-// //export default { MyComponent }; //This is not allowed
-
-// function MyComponent() {
-//   return <div>Hello</div>
-// }
+// MyComponent.jsx
+export function MyComponent() {
+  return <div>Hello</div>
+}
